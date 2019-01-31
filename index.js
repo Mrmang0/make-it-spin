ffdcc92726757c4678756dce0e621f8a061ab8fe
Mrmang0 <mrmango050@gmail.com>
2019-01-31 23:44:57 +0200

    created single pipeline for all draw functions

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 let state = {
     canvas: null,
     context: null,
-    delay: 500,
-
-
-    angle: {
-        value: Math.PI,
-        get: () => {
-            return this.value + 0, 2;
-        },
-    },
-
-
+    mainPipeline: null,
 
     misc: {
         getRndInteger: (min, max) => {
@@ -24,21 +14,18 @@ let state = {
 
         setRandomLineColor: () => {
             const ite = new state.classes.Iterator(1.0, 1.0);
-            state.setLineColor(
-                state.misc.getRndColor(), ite.get())
+            state.misc.setLineColor(
+                state.misc.getRndColor(), 1)
         },
 
         setWhiteLineColor: () => {
-            const ite = new state.classes.Iterator(0.1, 1.0);
-            state.setLineColor(
-                '255,255,255', ite.get())
+            state.misc.setLineColor(
+                '255,255,255', 1)
         },
 
-        setLineColor: (color) => {
-            const ite = new state.classes.Iterator(0.1, 1.0);
-            state.setLineColor(
-                color, ite.get())
-        }
+        setLineColor: async function (rgb, a) {
+            state.context.strokeStyle = `rgba(${rgb},${a})`;
+        },
     },
 
     screenResolutionCounter: function () {
@@ -66,32 +53,9 @@ let state = {
         this.context.strokeStyle = 'rgba(255,255,255,1.0)'
         this.context.globalAlpha = 1.0;
         this.context.lineWidth = 10;
+        this.mainPipeline = new this.classes.Pipeline(100);
     },
 
-    testDraw: async function () {
-        setInterval(() => {
-            const seed = this.iterator.get();
-            this.context.strokeStyle = `rgba(${state.misc.getRndColor()},${seed/10})`;
-            this.context.strokeRect(900, 500, 100, 100);
-        }, this.delay)
-    },
-
-    setLineColor: async function (rgb, a) {
-        this.context.strokeStyle = `rgba(${rgb},${a})`;
-    },
-
-    testRotation: async function () {
-        setInterval(() => {
-            this.context.rotate(state.angle.get());
-        }, this.delay);
-
-    },
-
-    singleTranslate: async function () {
-        const movX = 1.1;
-        const movY = 1;
-        this.context.translate(movX, movY);
-    },
     classes: {
         Arc: class DrawArc {
             constructor(x, y, radius, iterator, additionalFunc) {
@@ -104,26 +68,22 @@ let state = {
             }
 
             draw() {
-
-                state.WrapIntoInterval(() => {
-                    if (this.iterator.peek() >= 2 * Math.PI)
-                        return
-                    state.context.beginPath();
-                    if (this.additionalFunc) {
-                        this.additionalFunc();
-                    }
-                    state.context.arc(this.x, this.y, this.radius, this.iterator.get(), this.iterator.get());
-                    this.currentPoint = this.iterator.peek();
-                    state.context.stroke();
-                }, state.delay / this.x * this.radius);
-
+                if (this.iterator.peek() >= 2 * Math.PI)
+                    return
+                state.context.beginPath();
+                if (this.additionalFunc) {
+                    this.additionalFunc();
+                }
+                state.context.arc(this.x, this.y, this.radius, this.iterator.get(), this.iterator.get());
+                this.currentPoint = this.iterator.peek();
+                state.context.stroke();
             }
 
             getCurrentPoint() {
                 const angle = this.currentPoint;
                 const center = {
-                    x: this.x + this.radius,
-                    y: this.y + this.radius
+                    x: this.x,
+                    y: this.y 
                 }
 
                 const X = center.x + (this.radius * Math.cos(angle));
@@ -132,8 +92,6 @@ let state = {
                     X,
                     Y
                 };
-
-
             }
         },
 
@@ -144,7 +102,6 @@ let state = {
             }
 
             draw() {
-                console.log(state.context);
                 state.context.lineWidth = 2;
                 state.context.beginPath();
                 state.context.moveTo(this.startPoint.X, this.startPoint.Y);
@@ -172,54 +129,87 @@ let state = {
             }
 
             get() {
-                if (this.iterator > this.limitator)
-                    this.iterator = this.startValue;
+                if (this.customIncrement > 0) {
+                    if (this.iterator > this.limitator)
+                        this.iterator = this.startValue;
+                } else {
+                    if (this.iterator < this.limitator)
+                        this.iterator = this.startValue;
+                }
                 this.iterator = this.iterator + this.customIncrement;
+                console.log(this.iterator + this.customIncrement);
                 return this.iterator;
             }
         },
+
+        Pipeline: class Pipeline {
+            constructor(tickDelay) {
+                if (tickDelay)
+                    this.tickDelay = tickDelay;
+                else
+                    this.tickDelay = 100;
+                this.functions = [];
+                this.startPipeline();
+                this.interval;
+
+            }
+
+            startPipeline() {
+                const _this = this;
+                this.interval = setInterval(() => {
+                    for (let func of _this.functions) {
+                        if (func.currentIteration == func.iterations - 1)
+                            this.functions.splice(
+                                this.functions.indexOf(func)
+                            );
+
+                        func.execute();
+                        func.currentIteration++;
+                    }
+                }, this.tickDelay);
+            }
+
+            addFunction(func, iterations = -1) {
+                this.functions.push({
+                    execute: func,
+                    iterations: iterations,
+                    currentIteration: 0
+                })
+            }
+
+            stopPipeline() {
+                clearInterval(this.interval);
+            }
+        },
     },
-    WrapIntoInterval: (callFunction, delay) => {
-        setInterval(() => {
-            callFunction();
-        }, delay);
-    }
+
 
 }
-
-
 
 state.initialize();
-// state.context.strokeRect(200, 200, 200, 200)
-// state.testDraw();
-
-// let a = new state.classes.Arc(200, 200, 50,
-//     new state.classes.Iterator(Math.PI / 100, 2 * Math.PI, Math.PI / 100),
-//     state.misc.setRandomLineColor)
-
-
+state.mainPipeline.stopPipeline();
 const Circles = [];
-for (let i = 1; i < 6; i++) {
-    Circles.push(new state.classes.Arc(i * 85, i * 65, i * 10 + 5,
-        new state.classes.Iterator(Math.PI / 100, 2 * Math.PI, Math.PI / 100),
-        ()=>{state.misc.setLineColor('0,0,0')}))
-    Circles[i - 1].draw();
+
+
+for (let i = 1; i < 3; i++) {
+    Circles.push(
+        new state.classes.Arc(i * 100 * i, i * 10 * i, i * 10 + 5,
+            new state.classes.Iterator(Math.PI / 100, 2 * Math.PI, Math.PI / 100),
+            () => {
+                state.misc.setLineColor(`255,255,255`, 1)
+            }))
 }
 
+for (let i = 0; i < 1; i++) {
+    // const it = new state.classes.Iterator(0.8, 0.1, -0.01);
+    state.mainPipeline.addFunction(Circles[i].draw.bind(Circles[i]), 101);
+    state.mainPipeline.addFunction(Circles[i + 1].draw.bind(Circles[i + 1]), 101);
+    state.mainPipeline.addFunction(() => {
+        state.misc.setLineColor("255,0,0", 0.5);
+        new state.classes.Line(Circles[i].getCurrentPoint(),
+            Circles[i + 1].getCurrentPoint()).draw();
+    }, 101);
 
-// const b = new state.classes.Line( Circles[1].getCurrentPoint(),Circles[2].getCurrentPoint());
+}
 
-
-state.WrapIntoInterval(() => {
-    for (let i = 0; i < 4; i++) {
-       
-        const a = new state.classes.Line(Circles[i].getCurrentPoint(), Circles[i + 1].getCurrentPoint());
-        state.misc.setWhiteLineColor();
-        a.draw();
-    }
-
-}, state.delay + 1);
-
-// state.testTranslate();
-// state.testRotation();
-// state.testTranslate();
+state.mainPipeline.startPipeline();
